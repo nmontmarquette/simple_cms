177728a490e36f849d1d963bbf1c2fd79db871b7
Nicolas Montmarquette <nicolas.montmarquette@autodesk.com>
2017-08-07 19:22:52 -0400

    Add demo/index, demo/hello

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,18 @@
 class DemoController < ApplicationController
+  
+  layout false
+
   def index
+    render ('demo/index')
+  end
+
+  def hello
+    #render ('demo/index')
+    
+    # We're already in the 'Demo' controller no need to specify
+    #render ('demo/hello')
+    render ('hello')
+  end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  #get 'demo/index'
+
+  root 'demo#index'
+
+  get 'demo/hello'
   get 'demo/index'
+
+  # Default roule, may go ahead in the future
+  #get ':controller(/:action(/:id))'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
